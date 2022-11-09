ab38fc867b1c39300fcf183392d2acc8054bcf8a
Akash Sampurnanand Pandey <77798269+akash-pandey1729@users.noreply.github.com>
2022-11-08 22:18:21 -0500

    Time: 1072 ms (31.75%), Space: 19.5 MB (70.93%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.py
+++ 0901-online-stock-span/0901-online-stock-span.py
@@ -1,11 +1,12 @@
 class StockSpanner:
     def __init__(self):
         self.stack = []
-
+# If the number is small, keep appending it to the stack, if greator, keep popping till you actually reach the number greator than current number..append the curr with count
     def next(self, price: int) -> int:
         ans = 1
         while self.stack and self.stack[-1][0] <= price:
             ans += self.stack.pop()[1]
+            
         
         self.stack.append([price, ans])
         return ans
