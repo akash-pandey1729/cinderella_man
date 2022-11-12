7553da4d33e6e56832c51adc528769a5fffea304
Akash Sampurnanand Pandey <77798269+akash-pandey1729@users.noreply.github.com>
2022-11-12 11:14:43 -0500

    Time: 692 ms (20.00%), Space: 40.5 MB (60.00%) - LeetHub

---
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+        dp = defaultdict(int)
+        dp[0] = 1
+        mod = 10 ** 9 + 7
+        for i in range(1, high + 1):
+            dp[i] = (dp[i - zero] + dp[i - one]) % mod
+        return sum(dp[i] for i in range(low, high + 1)) % mod
+            
+            
+        
+        
