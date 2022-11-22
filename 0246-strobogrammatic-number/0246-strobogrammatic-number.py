834740e4b277c0b268f4a9fd5e7a7e1bab82316d
Akash Sampurnanand Pandey <77798269+akash-pandey1729@users.noreply.github.com>
2022-11-22 06:05:10 -0500

    Time: 43 ms (5.69%), Space: 13.6 MB (34.96%) - LeetHub

---
+++ 0246-strobogrammatic-number/0246-strobogrammatic-number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isStrobogrammatic(self, num):
+        """
+        :type num: str
+        :rtype: bool
+        """
+        maps = {("0", "0"), ("1", "1"), ("6", "9"), ("8", "8"), ("9", "6")}
+        i,j = 0, len(num) - 1
+        while i <= j:
+            if (num[i], num[j]) not in maps:
+                return False
+            i += 1
+            j -= 1
+        return True
+        
