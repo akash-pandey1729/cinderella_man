982e385e39ed1f946d513d22c2f91eaf869bc053
Akash Sampurnanand Pandey <77798269+akash-pandey1729@users.noreply.github.com>
2022-11-26 18:52:20 -0500

    Time: 927 ms (73.33%), Space: 85.4 MB (15.79%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -1,12 +1,20 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
         
-        dp = [[0,0]]                                            #   e     s     p     dp
-                                                                #  ––-–  ––-–  ––-–  ––––––––––––––––-–
-        f = lambda x: dp[bisect_left(dp,[x+1])-1][1]            #    3     1    20   [[0,0],[3,20]]
-                                                                #    5     2    20   [[0,0],[3,20],[5,20]]
-        for e, s, p in sorted(zip(endTime,startTime, profit)):  #    6     4    70   [[0,0],[3,20],[5,20],[6,90]]
-                                                                #    9     6    60   [[0,0],[3,20],[5,20],[6,90],[9,150]]
-            dp.append([e, max(f(e),p+f(s))])                    #   10     3   100   [[0,0],[3,20],[5,20],[6,90],[9,150],[10,150]]
-                                                                #                                                             |          
-        return dp[-1][1]    
+        arr = [[startTime[i], endTime[i], profit[i]] for i in range(len(profit))]
+        arr.sort(key = lambda x:x[0])
+        
+        dp = {}
+        def func(i=0):
+            if i>=len(arr):
+                return 0
+            if i not in dp:
+                j = bisect.bisect_left(arr, arr[i][1], key = lambda x:x[0])
+                dp[i]= max(func(i+1), arr[i][2]+ func(j))
+            return dp[i]
+        return func()
+            
+            
+            
+            
+        
