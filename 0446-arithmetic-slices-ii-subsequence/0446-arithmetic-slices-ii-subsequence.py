7fea0f661933ea624200e1c5b13c2641493e7b51
Akash Sampurnanand Pandey <77798269+akash-pandey1729@users.noreply.github.com>
2022-11-27 19:25:17 -0500

    Time: 1502 ms (75.00%), Space: 228.4 MB (10.36%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.py
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.py
@@ -1,13 +1,29 @@
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
-        
-        n = len(nums)
-        cnt = [defaultdict(int) for _ in range(n)]
-        
-        subseq = 0
-        for i,a in enumerate(nums):
-            for j,b in enumerate(nums[:i]):
-                cnt[i][a-b] += cnt[j][a-b] + 1
-                subseq += cnt[j][a-b]
-        
-        return subseq
+        import numpy as np 
+        #storing the indexes of the elements in dictionary a with values as a key
+        counter={}
+        for i in range(len(nums)):
+            if nums[i] not in counter:
+                counter[nums[i]]=[i]
+            else:
+                counter[nums[i]].append(i)
+        dp={}
+        def rec(i,d,c):
+            if (i,d,c) in dp:
+                return dp[(i,d,c)]
+            
+            total=0
+            if c>=3:
+                total+=1
+            if nums[i]+d in counter:
+                for j in counter[nums[i]+d]:
+                    if j>i:
+                        total+=rec(j,d,c+1)
+            dp[(i,d,c)]=total
+            return total
+        ans=0
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):             
+                ans+=rec(j,nums[j]-nums[i],2)
+        return ans
