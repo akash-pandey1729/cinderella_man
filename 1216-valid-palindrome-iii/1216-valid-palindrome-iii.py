ca9fc6015778f199acf81264fb3086700c541301
Akash Sampurnanand Pandey <77798269+akash-pandey1729@users.noreply.github.com>
2022-11-29 11:38:05 -0500

    Time: 1563 ms (36.13%), Space: 82.1 MB (44.38%) - LeetHub

--- 1216-valid-palindrome-iii/1216-valid-palindrome-iii.py
+++ 1216-valid-palindrome-iii/1216-valid-palindrome-iii.py
@@ -1,36 +1,15 @@
 class Solution:
     def isValidPalindrome(self, s: str, k: int) -> bool:
-        def lcs(X, Y):
-            m = len(X)
-            n = len(Y)
-            L = [[0 for i in range(n+1)] for j in range(m+1)]
-            for i in range(m+1):
-                for j in range(n+1):
-                    if i == 0 or j == 0:
-                        L[i][j] = 0
-                    elif X[i-1] == Y[j-1]:
-                        L[i][j] = L[i-1][j-1] + 1
-                    else:
-                        L[i][j] = max(L[i-1][j], L[i][j-1])
-            lcs = ""
-            i = m
-            j = n
-            while i > 0 and j > 0:
-                if X[i-1] == Y[j-1]:
-                    lcs += X[i-1]
-                    i -= 1
-                    j -= 1
-                elif L[i-1][j] > L[i][j-1]:
-                    i -= 1
-
+        dp  = {}
+        def func(i=0,j=len(s)-1):
+            if i>=j:
+                return 0
+            if (i,j) not in dp:
+                if s[i] == s[j]:
+                    dp[(i,j)] =  func(i+1,j-1)
                 else:
-                    j -= 1
-            lcs = lcs[::-1]
-            return lcs
-        # print(lcs(s,s[::-1]))
-        if len(lcs(s,s[::-1]))>= len(s) - k:
-            return True
-        return False
-        
-
+                    dp[(i,j)] =  1+ min(func(i,j-1), func(i+1,j))
+            return dp[(i,j)]
+        return func()<=k
+                
         
