e6a8a284da610c6e511835925753203489ca64a5
Akash Sampurnanand Pandey <77798269+akash-pandey1729@users.noreply.github.com>
2023-07-06 16:59:29 -0400

    Time: 39 ms (51.57%), Space: 8.6 MB (5.16%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.go
@@ -0,0 +1,21 @@
+func minSubArrayLen(target int, nums []int) int {
+    sum:=0
+    start:=0
+    ans:= len(nums)+1
+    for i:=0;i<len(nums);i++ {
+        sum = sum + nums[i]
+        // fmt.Println(sum, start, i, ans)
+        for sum>=target {
+            if ans > i - start + 1 {
+                ans = i-start + 1
+            }
+            sum = sum - nums[start]
+            start = start + 1
+        }
+    }
+    if ans == len(nums)+1{
+        return 0
+    }
+    return ans 
+    
+}
