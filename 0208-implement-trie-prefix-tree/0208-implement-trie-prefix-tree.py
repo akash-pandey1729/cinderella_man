ffce0778b5395868a6faa433566042dde1401cb9
Akash Sampurnanand Pandey <77798269+akash-pandey1729@users.noreply.github.com>
2023-09-10 16:56:23 -0400

    Time: 162 ms (53.71%), Space: 31 MB (78.16%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -0,0 +1,42 @@
+from collections import defaultdict
+class Trie:
+    def __init__(self):
+        self.root_dict = defaultdict(dict)
+        
+    def insert(self, word: str) -> None:
+        def help(word, dict1):
+            if not word:
+                dict1['word'] = True
+                return
+            if word[0] not in dict1:
+                dict1[word[0]] = {"word": False}
+            help(word[1:], dict1[word[0]])
+        help(word, self.root_dict)
+        # print(self.root_dict)
+        
+    def search(self, word: str) -> bool:
+        def help(word, dict1):
+            if not word and dict1['word']:
+                return True
+            if not word or word[0] not in dict1:
+                return False
+            return help(word[1:], dict1[word[0]])
+        return help(word, self.root_dict)
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        def help(word, dict1):
+            if not word:
+                return True
+            if not word or word[0] not in dict1:
+                return False
+            return help(word[1:], dict1[word[0]])
+        return help(prefix, self.root_dict) 
+        
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
