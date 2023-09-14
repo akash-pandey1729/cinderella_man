0608ebf5324eedc7b64a70803a9b16f1d2bf090f
Akash Sampurnanand Pandey <77798269+akash-pandey1729@users.noreply.github.com>
2023-09-14 18:18:08 -0400

    Time: 35 ms (80.51%), Space: 16.2 MB (91.94%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -2,8 +2,56 @@ class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         m = len(needle)
         n = len(haystack)
-        for i in range(n-m+1):
-            if haystack[i:i+m] == needle:
-                return i
+
+        if n < m:
+            return -1
+
+        # PREPROCESSING
+        # longest border array
+        longest_border = [0] * m
+        # Length of Longest Border for prefix before it.
+        prev = 0
+        # Iterating from index-1. longest_border[0] will always be 0
+        i = 1
+
+        while i < m:
+            if needle[i] == needle[prev]:
+                # Length of Longest Border Increased
+                prev += 1
+                longest_border[i] = prev
+                i += 1
+            else:
+                # Only empty border exist
+                if prev == 0:
+                    longest_border[i] = 0
+                    i += 1
+                # Try finding longest border for this i with reduced prev
+                else:
+                    prev = longest_border[prev - 1]
+
+        # SEARCHING
+        # Pointer for haystack
+        haystack_pointer = 0
+        # Pointer for needle.
+        # Also indicates number of characters matched in current window.
+        needle_pointer = 0
+
+        while haystack_pointer < n:
+            if haystack[haystack_pointer] == needle[needle_pointer]:
+                # Matched Increment Both
+                needle_pointer += 1
+                haystack_pointer += 1
+                # All characters matched
+                if needle_pointer == m:
+                    # m characters behind last matching will be window start
+                    return haystack_pointer - m
+            else:
+                if needle_pointer == 0:
+                    # Zero Matched
+                    haystack_pointer += 1
+                else:
+                    # Optimally shift left needle_pointer. 
+                    # Don't change haystack_pointer
+                    needle_pointer = longest_border[needle_pointer - 1]
+
         return -1
-        
