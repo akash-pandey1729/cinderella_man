bca149163be5f91ea0e2f18b779ec4d1f779931b
Akash Sampurnanand Pandey <77798269+akash-pandey1729@users.noreply.github.com>
2023-09-20 11:40:42 -0400

    Time: 303 ms (9.44%), Space: 16.3 MB (75.63%) - LeetHub

---
+++ 1873-longest-nice-substring/1873-longest-nice-substring.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestNiceSubstring(self, s: str) -> str:
+        def IsNice(str1):
+            cnt = Counter(str1)
+            for s in str1:
+                if s.upper() not in cnt or s.lower() not in cnt:
+                    return False
+            return True
+        ans = 0
+        # print(IsNice("Bb"))
+        temp = ""
+        for i in range(len(s)):
+            for j in range(i+1,len(s)+1):
+                if IsNice(s[i:j]) and ans<j-i + 1:
+                    ans = max(ans, j-i + 1)
+                    temp = s[i:j]
+        return temp 
