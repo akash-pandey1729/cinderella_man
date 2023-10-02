bc89dbf3c484a2cbf2b01a6c0d0f5f0ab8ff909d
Akash Sampurnanand Pandey <77798269+akash-pandey1729@users.noreply.github.com>
2023-10-02 00:04:42 -0400

    Time: 303 ms (12.3%), Space: 17.3 MB (65.73%) - LeetHub

---
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
@@ -0,0 +1,33 @@
+class Solution:
+    def winnerOfGame(self, colors: str) -> bool:
+        if len(colors)<=2:
+            return False
+        conseq_B = 0
+        moves_A = 0
+        moves_B = 0
+        prev = colors[0]
+        def getmoves(l1, l2):
+            moves = 0
+            prev = colors[0]
+            conseq = int(colors[0]==l1)
+            for i in range(1, len(colors)):
+                if prev == l1 and colors[i]==l1:
+                    conseq+=1
+                elif prev == l2 and colors[i]==l1:
+                    prev = l1
+                    conseq = 1
+                elif colors[i]==l2:
+                    prev = l2
+                    if conseq>=3:
+                        moves+= (conseq - 2)
+                    conseq = 0
+            if conseq>=3:
+                    moves+= (conseq - 2)
+            return moves
+        moves_A = getmoves('A', 'B')
+        moves_B = getmoves('B', 'A')
+        return moves_A>moves_B
+
+
+
+        
