0786bc2008ac10cca571b3849d8525504ebdebd6
Akash Sampurnanand Pandey <77798269+akash-pandey1729@users.noreply.github.com>
2023-10-04 01:38:52 -0400

    Time: 194 ms (78.24%), Space: 20 MB (58.72%) - LeetHub

---
+++ 0817-design-hashmap/0817-design-hashmap.py
@@ -0,0 +1,39 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.key_space = 2069
+        self.dict1 = [[] for i in range(self.key_space)]
+
+    def put(self, key: int, value: int) -> None:
+        idx = key%self.key_space
+        for i in range(len(self.dict1[idx])):
+            if self.dict1[idx][i][0]==key:
+                self.dict1[idx][i][1] = value
+                return 
+        self.dict1[idx].append([key, value])
+        return 
+
+
+
+    def get(self, key: int) -> int:
+        idx = key%self.key_space
+        for k,v in self.dict1[idx]:
+            if k==key:
+                return v
+        return -1
+    
+    def remove(self, key: int) -> None:
+        idx = key%self.key_space
+        for i in range(len(self.dict1[idx])):
+            if self.dict1[idx][i][0]==key:
+                del self.dict1[idx][i]
+                return 
+
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
