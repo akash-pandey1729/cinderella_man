a4525bc1e714b564870ee95499e70b7a67b82aa6
Akash Sampurnanand Pandey <77798269+akash-pandey1729@users.noreply.github.com>
2023-10-05 21:32:53 -0400

    Time: 290 ms (14.14%), Space: 19.6 MB (9.16%) - LeetHub

---
+++ 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.py
@@ -0,0 +1,39 @@
+class Solution:
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+        if len(customers)<=minutes:
+            return sum(customers)
+        sum_array = []
+        for num in customers:
+            if sum_array:
+                sum_array.append(sum_array[-1]+ num)
+            else:
+                sum_array.append(num)
+        satisfied = [0]* len(customers)
+        satisfied[0] = customers[0] if not grumpy[0] else 0
+        for i in range(1, len(customers)):
+            if not grumpy[i]:
+                satisfied[i] = satisfied[i-1] + customers[i]
+            else:
+                satisfied[i] = satisfied[i-1]
+        ans = 0
+        satisfied.insert(0,0)
+        sum_array.insert(0,0)
+        print(satisfied)
+        print(sum_array)
+        for i in range(len(customers)-minutes+1):
+            print("i is", i)
+            ans = max(ans, sum_array[i+minutes]-sum_array[i] + satisfied[i] + satisfied[-1]-satisfied[i+minutes])
+        return ans
+            
+            
+        [1,0,7,5,1,1,1,0]     
+        [0,1,0,1,0,1,0,1]   
+
+
+
+
+
+
+
+
+        
