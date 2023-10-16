78021203002bd50fcbef3a89916a458c5d7725ae
Akash Sampurnanand Pandey <77798269+akash-pandey1729@users.noreply.github.com>
2023-10-16 11:42:15 -0400

    Time: 78 ms (74.21%), Space: 19.8 MB (85.1%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -2,7 +2,6 @@ class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         heapq.heapify(intervals)
         rooms = []
-        heapq.heapify(rooms)
         ans = 0
         while intervals:
             while rooms and rooms[0]<= intervals[0][0]:
