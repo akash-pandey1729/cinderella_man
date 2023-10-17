1b23416cefdd4e2b4cdbcabe7d2b3adf9ce8f0d3
Akash Sampurnanand Pandey <77798269+akash-pandey1729@users.noreply.github.com>
2023-10-17 11:54:05 -0400

    Time: 298 ms (28.44%), Space: 30.3 MB (5.69%) - LeetHub

--- 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.py
+++ 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.py
@@ -1,19 +1,13 @@
 class Solution:
     def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
         indegree = [0 for i in range(n)]
-        outdegree = [0 for i in range(n)]
         for i in range(n):
             if leftChild[i]==-1 and rightChild[i]==-1: 
                 continue
-            outdegree[i]+=1
             if leftChild[i]!=-1:
                 indegree[leftChild[i]]+=1
             if rightChild[i]!=-1:
                 indegree[rightChild[i]]+=1
-        
-        # print(indegree)
-        # print(outdegree)
-        # print()
         visited = set()
         def isCycle(node):
             if node == -1:
@@ -27,13 +21,6 @@ class Solution:
         
         if 0 not in indegree:
             return False
-        # for i in indegree:
-        #     if i>1:
-        #         return False
-        # for i in outdegree:
-        #     if i>2:
-        #         return False
-
         if isCycle(indegree.index(0)):
             return False
         if len(visited)!=n:
