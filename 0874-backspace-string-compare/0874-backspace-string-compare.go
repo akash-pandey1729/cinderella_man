58408f4263c0c2d546ce8eba3870b730dc928078
Akash Sampurnanand Pandey <77798269+akash-pandey1729@users.noreply.github.com>
2023-10-18 21:26:18 -0400

    Time: 12 ms (17.39%), Space: 2.5 MB (6.21%) - LeetHub

---
+++ 0874-backspace-string-compare/0874-backspace-string-compare.go
@@ -0,0 +1,23 @@
+import ("fmt"; "bytes")
+
+func getFinalString(s string) []byte {
+        var slice []byte
+        for i:=0; i<len(s);i++{
+            fmt.Println(slice)
+            if len(slice)>0 && s[i]== '#'{
+                slice = slice[:len(slice)-1]
+            }
+            if s[i]=='#' {
+                continue
+            } else {
+                slice = append(slice, byte(s[i]))
+            }
+        }
+        return slice
+    }
+func backspaceCompare(s string, t string) bool {
+    // fmt.Println(getFinalString(s))
+    fmt.Println(getFinalString(t))
+    return bytes.Equal(getFinalString(s), getFinalString(t))
+    
+}
