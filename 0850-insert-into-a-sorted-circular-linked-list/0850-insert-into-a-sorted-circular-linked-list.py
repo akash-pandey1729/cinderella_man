172bd0b53ab92757842ffa99212c7481356567e1
Akash Sampurnanand Pandey <77798269+akash-pandey1729@users.noreply.github.com>
2023-10-21 17:21:24 -0400

    Time: 51 ms (10.5%), Space: 17.1 MB (82.44%) - LeetHub

---
+++ 0850-insert-into-a-sorted-circular-linked-list/0850-insert-into-a-sorted-circular-linked-list.py
@@ -0,0 +1,36 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    def insert(self, head: 'Optional[Node]', insertVal: int) -> 'Node':
+        if not head:
+            head = Node(insertVal, None)
+            head.next = head
+            return head
+        prev, curr = head, head.next
+        while True:
+            if prev.val<=insertVal and curr.val>= insertVal:
+                prev.next = Node(insertVal, curr)
+                return head
+            elif prev.val > curr.val:
+                if insertVal >= prev.val or insertVal <= curr.val:
+                    prev.next = Node(insertVal, curr)
+                    return head
+            prev = prev.next
+            curr = curr.next
+            if curr==head:
+                break
+        prev.next = Node(insertVal, curr)
+        return head
+        
+
+
+
+
+
+        
