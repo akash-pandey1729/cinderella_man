a618d6dc85ecd6dfcb51bb67eca92b4f060323d8
Akash Sampurnanand Pandey <77798269+akash-pandey1729@users.noreply.github.com>
2023-10-24 13:07:06 -0400

    Time: 154 ms (22.36%), Space: 30.9 MB (51.86%) - LeetHub

--- 1796-correct-a-binary-tree/1796-correct-a-binary-tree.py
+++ 1796-correct-a-binary-tree/1796-correct-a-binary-tree.py
@@ -1,33 +1,38 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def correctBinaryTree(self, root: TreeNode) -> TreeNode:
-        stack = [root]
-        parent = defaultdict()
-        parent[root] = root
-        while stack:
-            n = len(stack)
-            stack_set = set(stack)
-            for node in stack:
-                if node.right in stack_set:
-                    if parent[node].left==node:
-                        parent[node].left = None
-                        return root
-                    else:
-                        parent[node].right = None
-                        return root
-            for i in range(n):
-                temp = stack.pop(0)
-                if temp.left:
-                    parent[temp.left] = temp
-                    stack.append(temp.left)
-                if temp.right:
-                    parent[temp.right] = temp
-                    stack.append(temp.right)
-        return root
+        # Queue for BFS. Every element stores [node, parent]
+        queue = deque([[root, None]])
 
-        
+        # Traverse Level by Level
+        while queue:
+            # Nodes in the current level
+            n = len(queue)
+
+            # Hash Set to store nodes of the current level
+            visited = set()
+            print(visited)
+
+            # Traverse all nodes in the current level
+            for _ in range(n):
+                # Pop the node from the queue
+                node, parent = queue.popleft()
+
+                # If node.right is already visited, then the node is defective
+                if node.right in visited:
+                    # Replace the child of the node's parent with null and return the root
+                    if parent.left == node:
+                        parent.left = None
+                    else:
+                        parent.right = None
+                    return root
+
+                # Add node to visited
+                visited.add(node)
+
+                # Add child in queue for traversal in next level
+                # They won't get popped in this level because of "n"
+                # Add the right child first, so that we can explore right to left
+                if node.right:
+                    queue.append([node.right, node])
+                if node.left:
+                    queue.append([node.left, node])
