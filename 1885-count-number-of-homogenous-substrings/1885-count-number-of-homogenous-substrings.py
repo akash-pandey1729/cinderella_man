67828b040e32d00f9d32c557e13e192a2842b134
Akash Sampurnanand Pandey <77798269+akash-pandey1729@users.noreply.github.com>
2023-11-09 18:20:05 -0500

    Time: 124 ms (54.93%), Space: 17.4 MB (30.99%) - LeetHub

--- 1885-count-number-of-homogenous-substrings/1885-count-number-of-homogenous-substrings.py
+++ 1885-count-number-of-homogenous-substrings/1885-count-number-of-homogenous-substrings.py
@@ -1,16 +1,13 @@
 class Solution:
     def countHomogenous(self, s: str) -> int:
-        l,r = 0,0
         ans = 0
-        while r<len(s):
-            # print(r,l)
-            if s[l]!=s[r]:
-                ans+= (r-l)*(r-l+1)//2
-                l=r
-            r+=1
-        print(r,l, "here")
-        ans+=(r-l)*(r-l+1)//2
-        return ans%(10**9 + 7)
-                
-
+        MOD = (10**9 + 7)
+        streak = 1
+        for i in range(1,len(s)):
+            if s[i]==s[i-1]:
+                streak+=1
+            else:
+                streak = 1
+            ans+= streak
+        return (ans+1)%MOD
         
