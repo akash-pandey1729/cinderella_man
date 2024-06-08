e8dc673e5d0be4ec98b942be930ebeb689597b06
Akash Sampurnanand Pandey <77798269+akash-pandey1729@users.noreply.github.com>
2024-06-07 19:28:31 -0500

    Time: 89 ms (80.74%), Space: 18.3 MB (10.39%) - LeetHub

--- 1648-minimum-insertions-to-balance-a-parentheses-string/1648-minimum-insertions-to-balance-a-parentheses-string.py
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/1648-minimum-insertions-to-balance-a-parentheses-string.py
@@ -1,7 +1,7 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
         s = s.replace("))", "#")
-        # three cases
+        # three cases ch can be ( or # or (
         ans = 0
         stack = []
         for ch in s:
@@ -12,11 +12,13 @@ class Solution:
                     ans+=1
                     stack.pop()
                 elif not stack:
+                    # We need one open and one close bracket
                     ans+=2
             if ch == "#":
                 if stack:
                     stack.pop()
                 elif not stack:
+                    # we need one open bracket
                     ans+=1
         ans += 2*(len(stack))
         return ans
