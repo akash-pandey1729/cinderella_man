a5adc1d24cc9839addc32eda4df8d770fb04ce62
Akash Sampurnanand Pandey <77798269+akash-pandey1729@users.noreply.github.com>
2024-06-11 21:37:56 -0500

    Time: 48 ms (5.24%), Space: 16.6 MB (49.58%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
@@ -1,8 +1,6 @@
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
         i = 0
-        if len(word)<len(abbr):
-            return False
         abbr = list(abbr)
         while i<len(word) and abbr:
             t = abbr.pop(0)
@@ -10,7 +8,6 @@ class Solution:
                 return False
             while t.isdigit() and abbr and abbr[0].isdigit():
                 t+= abbr.pop(0)
-            print(t,i)
             if t.isdigit():
                 i+= int(t)
             else:
@@ -18,7 +15,6 @@ class Solution:
                     i+=1
                 else:
                     return False
-        # print(i)
         if abbr or i!=len(word):
             return False
         return True
