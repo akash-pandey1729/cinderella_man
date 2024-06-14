74c4ee6606fd936507c3e7895c8a0e71ad337909
Akash Sampurnanand Pandey <77798269+akash-pandey1729@users.noreply.github.com>
2024-06-14 12:00:11 -0500

    Time: 136 ms (88.35%), Space: 23.1 MB (5.01%) - LeetHub

--- 0912-random-pick-with-weight/0912-random-pick-with-weight.py
+++ 0912-random-pick-with-weight/0912-random-pick-with-weight.py
@@ -1,28 +1,15 @@
 class Solution:
 
     def __init__(self, w: List[int]):
-        self.w = w
-        self.range_arr = [() for i in w]
-        # w.sort()
-        self.range_arr[0] = (0,w[0]-1)
-        for i in range(1, len(w)):
-            self.range_arr[i] = (self.range_arr[i-1][1]+1,self.range_arr[i-1][1]+1+ w[i]-1)
-        print(self.range_arr)
-        self.sum_ = sum(w)
-
-    def pickIndex(self) -> int:
-        # check for boundaries
-        # [0,2]
-        r1 = random.randint(0, self.sum_-1)
-        idx = bisect.bisect_left(self.range_arr,(r1,))
-        if idx== len(self.range_arr) or r1<self.range_arr[idx][0]:
-            idx-=1
-        print(r1, idx)
-        return idx
-
+        self.weight = w
+        self.tots = sum(w)
+        self.mapping = []
+        last_idx = 0
+        for i in range(len(w)):
+            self.mapping.append((last_idx, i))
+            last_idx = last_idx+w[i]
         
-
-
-# Your Solution object will be instantiated and called as such:
-# obj = Solution(w)
-# param_1 = obj.pickIndex()
+    def pickIndex(self) -> int:
+        idx = random.randint(0, self.tots-1)
+        j = (bisect.bisect_right(self.mapping, (idx,)))
+        return self.mapping[j-1][1]
