9c994ce4a079de65806503e9ea8ca816c4b77cad
Akash Sampurnanand Pandey <77798269+akash-pandey1729@users.noreply.github.com>
2024-06-24 19:58:39 -0500

    Time: 40 ms (21.92%), Space: 16.6 MB (50%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,17 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def func(x,n):
+            if n==0:
+                return 1
+            else:
+                if n%2: #n is odd
+                    t = func(x,n//2)
+                    return x*t*t
+                else:
+                    t = func(x,n//2)
+                    return t*t
+        if n<0:
+            ans = func(x,abs(n))
+            return 1/ans
+        else:
+            return func(x,abs(n))
